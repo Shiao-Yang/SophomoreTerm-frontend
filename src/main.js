4ac3c1f480643fc9fa7c0882b7bcd906ce29d8ab
shiaoyang <2359900215@qq.com>
2022-08-04 09:57:36 +0800

    修改bug

--- src/main.js
+++ src/main.js
@@ -14,10 +14,6 @@ Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.prototype.eventbus = new Vue()
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI)
-
 new Vue({
   router,
   store,
